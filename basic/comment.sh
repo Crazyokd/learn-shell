ebdf81bd91c2109fbce886679de493296ed136b5
Rekord <1324596506@qq.com>
2022-02-08 16:24:02 +0800

    feat:add comment.sh

---
+++ basic/comment.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# single line comment
+# echo this is single line comment.
+
+# multi line comment
+:<<EOF
+echo this is multi line comment.
+echo this is multi line comment.
+echo this is multi line comment.
+EOF
