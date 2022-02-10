0d6c1e2e491debe984be33c28b6adffda649fffe
Rekord <1324596506@qq.com>
2022-02-10 20:24:52 +0800

    chore:complement echo

--- basic/echo.sh
+++ basic/echo.sh
@@ -3,6 +3,19 @@
 # output common string
 echo "Hello,World!"
 
+# output multi-line string
+echo "<HTML>
+    <HEAD>
+          <TITLE>Page Title</TITLE>
+    </HEAD>
+    <BODY>
+          Page body.
+    </BODY>
+</HTML>"
+
+# multiple commands are separated by ; in a single line
+echo a;echo b   # even if the previous command fails,the following command will still be executed normally.
+
 # output escape character
 echo "Hello,\"Rekord\""
 
@@ -12,13 +25,20 @@ echo "Hello,\"$name\""
 
 # output multi line string
 echo "Yes\nNo"
-
-# open escape character and output multi line string
+# open escape character
 echo -e "Yes\nNo"
+echo -e 'Yes\nNo'
 
+# cancel output newline
+echo -n "YES"
 # open escape character and don't output multi line string
 echo -e "Yes\c"
 echo "No"
+
+# one command multiple lines write,use \ to separate
+echo foo bar
+echo foo \
+bar
 
 # redirect output to file
 echo "test redirect" > test.txt
